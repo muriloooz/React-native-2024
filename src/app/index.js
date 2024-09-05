9cf1bf3a04a1f05c8875fbb2a8c8ef5cf211ca91
muriloooz <141411749+muriloooz@users.noreply.github.com>
2024-09-05 12:52:28 +0000

    commit

---
+++ src/app/index.js
@@ -0,0 +1,51 @@
+import { StatusBar } from "expo-status-bar";
+import { BackHandler, StyleSheet, Text, View } from "react-native";
+import { useAuth } from "../hooks/Auth";
+import { router } from "expo-router";
+
+export default function App() {
+  const { signIn, signOut } = useAuth();
+
+  const handleEntrarSuper = async () => {
+    try {
+      await signIn({ email: "super@email.com", password: "Super123!" });
+      router.replace("/");
+    } catch (error) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style>Minha primeira janela</Text>
+      <Button title="Signin Super" onPress={handleEntrarSuper} />
+      <Button
+        title="Signin Adm"
+        onPress={() => signIn({ email: "adm@email.com", password: "Adm123!" })}
+      />
+      <Button
+        title="Signin User"
+        onPress={() =>
+          signIn({ email: "user@email.com", password: "User123!" })
+        }
+      />
+      <Button title="Sobre" onPress={() =>router.push("/about")} />
+        <Button title="Sair do Aplicativo" onPress={()=>BackHandler.exitApp}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 15,
+  },
+  title: {
+    fontFamily: "regular",
+    fontSize: 20,
+  },
+});
